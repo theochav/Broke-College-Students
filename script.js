f05df9c7d1f14bb7d1edb6415b6ae67dc25bd511
theochav <theochav1228@gmail.com>
2026-05-13 18:59:37 -0400

    Clear button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function generateRecipe(ingredients, mealType){
+function generateRecipe(ingredients, mealType, people){
+    if (ingredients === "") return "Please input your ingredients.";
     if (mealType === "breakfast"){
         return (  
             "Recipe Idea: " + mealType + "\n" +
+            "Recipe for " + people + " people\n" +
             "Use these ingredients: " + ingredients + "\n" +
             "Step 1: Gather your ingredients.\n" +
             "Step 2: Cook the ingredients based on the recipe.\n" +
@@ -10,6 +12,7 @@ function generateRecipe(ingredients, mealType){
     } else if (mealType === "lunch"){
         return (
             "Recipe Idea: " + mealType + "\n" +
+            "Recipe for " + people + " people\n" +
             "Use these ingredients: " + ingredients + "\n" +
             "Step 1: Gather your ingredients.\n" +
             "Step 2: Cook the ingredients based on the recipe.\n" +
@@ -19,6 +22,7 @@ function generateRecipe(ingredients, mealType){
     } else if (mealType === "dinner"){
         return (
             "Recipe Idea: " + mealType + "\n" +
+            "Recipe for " + people + " people\n" +
             "Use these ingredients: " + ingredients + "\n" +
             "Step 1: Gather your ingredients.\n" +
             "Step 2: Cook ingredients based on the recipe.\n" +
@@ -26,12 +30,13 @@ function generateRecipe(ingredients, mealType){
         ) ;
     } else if (mealType === "snack"){
         return ("Recipe Idea: " + mealType + "\n" +
+        "Recipe for " + people + " people\n" +
         "Use these ingredients: " + ingredients + "\n" +
         "Step 1: Gather your ingredients.\n" +
         "Step 2: Cook ingredients based on the recipe.\n" +
         "Step 3: Enjoy."
         );
-    } else {
+    } else { 
      return "Please re-enter your statement.";
     }
 }
@@ -40,10 +45,19 @@ const ingredientsInput = document.getElementById("ingredientsInput");
 const mealTypeSelect = document.getElementById("mealTypeSelect");
 const generateButton = document.getElementById("generateButton");
 const recipeOutput = document.getElementById("recipeOutput");
+const peopleInput = document.getElementById("peopleInput");
+const clearButton = document.getElementById("clearButton");
 
 generateButton.addEventListener("click", function () {
     const ingredients = ingredientsInput.value; 
     const mealType = mealTypeSelect.value.toLowerCase();
-    const recipe = generateRecipe(ingredients, mealType);
+    const people= peopleInput.value;
+    const recipe = generateRecipe(ingredients, mealType, people);
     recipeOutput.textContent = recipe;
 }); 
+
+clearButton.addEventListener("click", function () {
+    ingredientsInput.value = "";
+    peopleInput.value = "";
+    recipeOutput.textContent = "";  
+})
