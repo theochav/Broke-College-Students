03e54ad352811ea91c353092349eb652757a5232
theochav <theochav1228@gmail.com>
2026-06-23 19:37:44 -0400

    ahdhddahdgahdhfagh

--- webpage.js
+++ webpage.js
@@ -2,20 +2,13 @@ const recipeOutput = document.getElementById("recipeOutput");
 
 const savedRecipe = localStorage.getItem("generatedRecipe");
 
-const clearBtn = document.getElementById('clear-recipe-btn'); 
-const button = document.querySelector('.my-button');
-button.addEventListener('click', function(event) {
-    console.log('Button was clicked!');
-  });  
-  const clearButton = document.querySelector('.clear-btn');
+const clearBtn = document.getElementById('clearRecipeButton'); 
 
-clearButton.addEventListener('click', () => {
-  // 1. Clear the recipe from localStorage (from our previous step)
-  localStorage.removeItem('savedRecipe'); 
-  
-  // 2. Redirect to your other HTML file
+clearBtn.addEventListener('click', function(event) {
+  localStorage.removeItem('generatedRecipe');   
+  recipeOutput.textContent = "No recipe has been genereated yet."
   window.location.href = 'index.html'; 
-});
+  });  
 
 if (savedRecipe !== null) {
     recipeOutput.textContent = savedRecipe;
