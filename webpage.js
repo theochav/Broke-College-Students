6131b577f5460218030e169de738c9c5207915b3
theochav <theochav1228@gmail.com>
2026-06-23 17:34:05 -0400

    gadhgasdbhdwnvadvhsa

--- webpage.js
+++ webpage.js
@@ -2,6 +2,21 @@ const recipeOutput = document.getElementById("recipeOutput");
 
 const savedRecipe = localStorage.getItem("generatedRecipe");
 
+const clearBtn = document.getElementById('clear-recipe-btn'); 
+const button = document.querySelector('.my-button');
+button.addEventListener('click', function(event) {
+    console.log('Button was clicked!');
+  });  
+  const clearButton = document.querySelector('.clear-btn');
+
+clearButton.addEventListener('click', () => {
+  // 1. Clear the recipe from localStorage (from our previous step)
+  localStorage.removeItem('savedRecipe'); 
+  
+  // 2. Redirect to your other HTML file
+  window.location.href = 'index.html'; 
+});
+
 if (savedRecipe !== null) {
     recipeOutput.textContent = savedRecipe;
 }
